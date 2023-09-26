728a99d96b83575962a2c39b1c1d937b45a38f1c
Julio-Azevedo <julioazevedodev@gmail.com>
2023-09-26 15:18:17 -0300

    Adicionando a estrutura de informação ao cliente.h

--- modules/clientes.h
+++ modules/clientes.h
@@ -1,3 +1,15 @@
+// estrutura de informações de um cliente
+typedef struct {
+    char nome[50];
+    char cpf[15];
+    char estado[2];
+    char cidade[40];
+    char endereco[120];
+    int telefone[12];
+} Clientes;
+
+// chamada das funções
 void modulo_clientes(void);
 char tela_clientes(void);
 void tela_cadastra_cliente(void);
+void tela_lista_cliente(void);
