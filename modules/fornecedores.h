642b191592475767706adf496696ea5c9d185d8b
Julio-Azevedo <julioazevedodev@gmail.com>
2023-09-26 15:20:48 -0300

    adiciona estrutura de informação ao fornecedores.h

--- modules/fornecedores.h
+++ modules/fornecedores.h
@@ -1,3 +1,15 @@
+// estrutura de informações de um fornecedor
+typedef struct {
+    char nome[50];
+    int CNPJ[20];
+    int telefone[12];
+    char estado[2];
+    char cidade[40];
+    char endereco[120];
+    char email[50];
+} Fornecedores;
+
+// chamada das funções
 void modulo_fornecedores(void);
 char tela_fornecedores(void);
 void tela_cadastra_fornecedor(void);
