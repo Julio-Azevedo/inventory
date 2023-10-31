ea5e3c82896a9298647ea0f5648e23126abc924e
Julio-Azevedo <julioazevedodev@gmail.com>
2023-10-30 23:57:25 -0300

    adicionando validação de digito e telefone

--- modules/util.c
+++ modules/util.c
@@ -70,3 +70,31 @@ int valida_cpf(char *cpf)
     return 1;
 }    
 // FEITO POR: EDUARDO EDSON /// GIT: https://gist.github.com/eduardoedson
+
+// validação de digitos numericos
+int ehDigito(char c) {
+  if (c >= '0' && c <= '9') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+/////////////////////////////////////////////////////////////////////////////
+/// Flavius Gorgonio: https://github.com/flgorgonio/linguasolta_2020.2
+
+// validação de telefone
+int valida_telefone(char* telefone) {
+  int tam;
+  tam = strlen(telefone);
+  if (tam != 13) {
+    return 0;
+  }
+  for (int i = 0; i < tam; i++) {
+    if (!ehDigito(telefone[i])) {
+        return 0;
+    }
+  }
+  return 1;
+}
+/////////////////////////////////////////////////////////////////////////////
+/// Adaptado de Flavius Gorgonio: https://github.com/flgorgonio/linguasolta_2020.2
