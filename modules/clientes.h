6e38b7096a70ab75c63370fd2ce32f0fbf9916cb
Julio-Azevedo <julioazevedodev@gmail.com>
2023-10-30 23:30:31 -0300

    chamando as funções do clientes.c

--- modules/clientes.h
+++ modules/clientes.h
@@ -1,15 +1,16 @@
 // estrutura de informações de um cliente
 typedef struct {
-    char nome[50];
-    char cpf[15];
-    char estado[2];
-    char cidade[40];
-    char endereco[120];
-    int telefone[12];
+    char nome[51];
+    char cpf[12];
+    char cidade[21];
+    char estado[3];
+    char endereco[100];
+    char telefone[14];
 } Clientes;
 
 // chamada das funções
 void modulo_clientes(void);
 char tela_clientes(void);
-void tela_cadastra_cliente(void);
+Clientes* tela_cadastro_cliente(void);
 void tela_lista_cliente(void);
+void grava_cliente(Clientes*);
