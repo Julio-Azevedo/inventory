56379134ec1da8cc6f438b3349e4cb7196daa07b
Julio-Azevedo <julioazevedodev@gmail.com>
2023-11-07 13:15:13 -0300

    feita as chamadas das validações

--- modules/util.h
+++ modules/util.h
@@ -5,3 +5,7 @@ int valida_cpf(const char *cpf);
 int valida_telefone(const char *telefone);
 int ehDigito(char c);
 int verifica_cpf_cliente(const char *cpf);
+int valida_cnpj(const char *cnpj);
+int verifica_cnpj_fornecedor(const char* cnpj);
+int valida_cep(const char* cep);
+int valida_email(char *email );
