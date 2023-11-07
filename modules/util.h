1e394ef65152a15af91f16170a35e0f86f00c3f2
Julio-Azevedo <julioazevedodev@gmail.com>
2023-11-07 03:24:04 -0300

    adicionada a chamada das funções de verificação

--- modules/util.h
+++ modules/util.h
@@ -1,6 +1,7 @@
 // chamando as funções de funcionalidade
 void limpa_buffer(void);
 int valida_nome(const char *nome);
-int valida_cpf(char *cpf);
-int valida_telefone(char* telefone);
+int valida_cpf(const char *cpf);
+int valida_telefone(const char *telefone);
 int ehDigito(char c);
+int verifica_cpf_cliente(const char *cpf);
