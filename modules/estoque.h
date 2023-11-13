39d7cbcc52fa3881ff4ab3af0915c8434543e376
Julio-Azevedo <julioazevedodev@gmail.com>
2023-11-13 10:53:09 -0300

    melhoria na organização e identação do arquivo estoque

--- modules/estoque.h
+++ modules/estoque.h
@@ -1,2 +1,11 @@
+#if !defined(ESTOQUE_H)
+#define ESTOQUE_H
+
+// declaração das funções
 void modulo_estoque(void);
+void modulo_clientes(void);
+void modulo_fornecedores(void);
+void modulo_produtos(void);
 char tela_estoque(void);
+
+#endif // ESTOQUE_H
