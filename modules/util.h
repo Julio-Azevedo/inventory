c2cc1b8ee8bf936e96a3f3c8c00ca2df892c8447
Julio-Azevedo <julioazevedodev@gmail.com>
2023-11-13 10:56:31 -0300

    adicionando a chamada da função controle de menu

--- modules/util.h
+++ modules/util.h
@@ -1,11 +1,13 @@
 // chamando as funções de funcionalidade
 void limpa_buffer(void);
+void limpar_tela(void);
 int valida_nome(const char *nome);
 int valida_cpf(const char *cpf);
-int valida_telefone(const char *telefone);
 int ehDigito(char c);
+int valida_telefone(const char *telefone);
 int verifica_cpf_cliente(const char *cpf);
 int valida_cnpj(const char *cnpj);
 int verifica_cnpj_fornecedor(const char* cnpj);
 int valida_cep(const char* cep);
 int valida_email(char *email );
+char controle_menu(void);
