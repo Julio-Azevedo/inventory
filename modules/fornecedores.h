72d02c65ba4c581b6f60423efd4fbc7bb62576eb
Julio-Azevedo <julioazevedodev@gmail.com>
2023-11-13 10:54:07 -0300

    melhoria na organização e identação do arquivo

--- modules/fornecedores.h
+++ modules/fornecedores.h
@@ -1,5 +1,9 @@
+#if !defined(FORNECEDORES_H)
+#define FORNECEDORES_H
+
 // estrutura de informações de um fornecedor
-typedef struct {
+typedef struct
+{
     char nome[51];
     char cnpj[21];
     char cidade[51];
@@ -10,15 +14,19 @@ typedef struct {
     int status;
 } Fornecedores;
 
-// chamada das funções
+// declaração das funções
 void modulo_fornecedores(void);
 char tela_fornecedores(void);
 Fornecedores *tela_cadastra_fornecedor(int editando);
+Fornecedores *tela_pesquisa_fornecedor(void);
+void tela_edita_fornecedor(void);
+void tela_excluir_fornecedor(void);
+void tela_listar_fornecedor(void);
+
+// declaração das funções auxiliares
 void grava_fornecedor(Fornecedores *fornecedor);
 void exibe_fornecedor(Fornecedores *fornecedor);
-Fornecedores *tela_pesquisa_fornecedor(void);
-void tela_listar_fornecedor(void);
 void regrava_fornecedor(Fornecedores *fornecedor);
-void tela_edita_fornecedor(void);
 Fornecedores *busca_cnpj_fornecedor(const char *cnpj);
-void tela_excluir_fornecedor(void);
+
+#endif // FORNECEDORES_H
